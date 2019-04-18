62cc701a5dc57c9823a81fb82fe0ae6a99529d13
Nishant Jain <nishantkumarajain@gmail.com>
2019-04-19 00:41:02 +0530

    Working Web Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements'
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
   ],
+  entryComponents: [ButtonComponent],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const customElement = createCustomElement(ButtonComponent, { injector: this.injector });
+    customElements.define('app-button', customElement);
+  }
+}
